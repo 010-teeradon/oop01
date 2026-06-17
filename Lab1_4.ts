6322908f895e2b34aec631a1077f3861ee97f29a
010 teeradon <684245010@webmail.npru.ac.th>
2026-06-17 17:00:22 +0700

    ส่งงานในห้อง Lab1_4.ts

--- Lab1_4.ts
+++ Lab1_4.ts
@@ -1,9 +1,8 @@
 let gross: number = 50000;
 let rate: number = 7;
-let paid: number=3500;
-console.log("Gross Income $%d",gross);
-console.log("Tax Rate: %d%",rate);
-console.log("Tex paid: %d",paid);
 let tex: number= gross*0.07;
 let Net: number= gross-tex;
+console.log("Gross Income $%d",gross);
+console.log("Tax Rate: %d%",rate);
+console.log("Tex paid: %d",tex);
 console.log("Net Income after Tax: %d",Net);
